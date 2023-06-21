5d909ed950d218cf34c4188d0f052a62acc0a150
Rinat <rinat-yar@mail.ru>
2023-06-21 22:56:46 +0300

    Initial commit_3

--- src/test/java/ru/netology/test/CashBackHackTest.java
+++ src/test/java/ru/netology/test/CashBackHackTest.java
@@ -3,71 +3,92 @@ package ru.netology.test;
 import ru.netology.CashBackHack;
 public class CashBackHackTest {
     @org.junit.Test
-    public void testForPurchaseOn1500() {
-        CashBackHackTest service = new CashBackHackTest();
-        int amount = 1_500;
+    public void testPurchase() {
+
+        CashBackHack service = new CashBackHack();
+
+        int amount = 1500;
         int expected = 500;
         int actual = service.remain(amount);
+
         org.junit.Assert.assertEquals(actual, expected);
     }
 
     @org.junit.Test
-    public void testForPurchaseOn1() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void testPurchase_1() {
+
+        CashBackHack service = new CashBackHack();
+
         int amount = 1;
         int expected = 999;
         int actual = service.remain(amount);
+
         org.junit.Assert.assertEquals(actual, expected);
     }
 
     @org.junit.Test
-    public void testForPurchaseOn0() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void testPurchase_2() {
+
+        CashBackHack service = new CashBackHack();
+
         int amount = 0;
         int expected = 1000;
         int actual = service.remain(amount);
+
         org.junit.Assert.assertEquals(actual, expected);
     }
 
     @org.junit.Test
-    public void testForPurchaseOn1000() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void testPurchase_3() {
+
+        CashBackHack service = new CashBackHack();
+
         int amount = 1000;
         int expected = 0;
         int actual = service.remain(amount);
+
         org.junit.Assert.assertEquals(actual, expected);
     }
 
     @org.junit.Test
-    public void testForPurchaseOn999() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void testPurchase_4() {
+
+        CashBackHack service = new CashBackHack();
+
         int amount = 999;
         int expected = 1;
         int actual = service.remain(amount);
+
         org.junit.Assert.assertEquals(actual, expected);
 
     }
 
     @org.junit.Test
-    public void testForPurchaseOn1001() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void testPurchase_5() {
+
+        CashBackHack service = new CashBackHack();
+
         int amount = 1001;
         int expected = 999;
         int actual = service.remain(amount);
+
         org.junit.Assert.assertEquals(actual, expected);
     }
 
     @org.junit.Test
-    public void testForPurchaseOn2000() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void testPurchase_6() {
+
+        CashBackHack service = new CashBackHack();
+
         int amount = 2000;
         int expected = 0;
         int actual = service.remain(amount);
+
         org.junit.Assert.assertEquals(actual, expected);
     }
     @org.junit.jupiter.api.Test
-    public void junit5testForPurchaseOn1500() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void junit5test_1500() {
+        CashBackHack service = new CashBackHack();
         int amount = 1_500;
         int expected = 500;
         int actual = service.remain(amount);
@@ -75,8 +96,8 @@ public class CashBackHackTest {
     }
 
     @org.junit.jupiter.api.Test
-    public void junit5testForPurchaseOn1() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void junit5test_1() {
+        CashBackHack service = new CashBackHack();
         int amount = 1;
         int expected = 999;
         int actual = service.remain(amount);
@@ -84,8 +105,8 @@ public class CashBackHackTest {
     }
 
     @org.junit.jupiter.api.Test
-    public void junit5testForPurchaseOn0() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void junit5test_0() {
+        CashBackHack service = new CashBackHack();
         int amount = 0;
         int expected = 1000;
         int actual = service.remain(amount);
@@ -93,8 +114,8 @@ public class CashBackHackTest {
     }
 
     @org.junit.jupiter.api.Test
-    public void junit5testForPurchaseOn1000() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void junit5test_1000() {
+        CashBackHack service = new CashBackHack();
         int amount = 1000;
         int expected = 0;
         int actual = service.remain(amount);
@@ -102,8 +123,8 @@ public class CashBackHackTest {
     }
 
     @org.junit.jupiter.api.Test
-    public void junit5testForPurchaseOn999() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void junit5test_999() {
+        CashBackHack service = new CashBackHack();
         int amount = 999;
         int expected = 1;
         int actual = service.remain(amount);
@@ -112,8 +133,8 @@ public class CashBackHackTest {
     }
 
     @org.junit.jupiter.api.Test
-    public void junit5testForPurchaseOn1001() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void junit5test_1001() {
+        CashBackHack service = new CashBackHack();
         int amount = 1001;
         int expected = 999;
         int actual = service.remain(amount);
@@ -121,8 +142,8 @@ public class CashBackHackTest {
     }
 
     @org.junit.jupiter.api.Test
-    public void junit5testForPurchaseOn2000() {
-        CashBackHackTest service = new CashBackHackTest();
+    public void junit5test_2000() {
+        CashBackHack service = new CashBackHack();
         int amount = 2000;
         int expected = 0;
         int actual = service.remain(amount);
